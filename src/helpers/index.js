bd7884915f9e6d708770f1eaf7887b218607a47c
Shomari Roberts <shomariroberts@gmail.com>
2022-04-15 11:23:04 -0400

    created getVideoById helper function

---
+++ src/helpers/index.js
@@ -0,0 +1,15 @@
+import youtube from '../api/youtube';
+
+export const getVideoById = async (value, setResponse, setError) => {
+  const response = await youtube.get('/videos', {
+    params: {
+      id: value,
+    },
+  });
+
+  if (response.data.items.length === 0) {
+    setError('Video not found, try another id');
+  } else {
+    setResponse(response.data.items);
+  }
+};
