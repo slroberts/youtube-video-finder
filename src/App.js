00d3dfafbfe9ec7c5e0a41a45e92b3c731d923ec
Shomari Roberts <shomariroberts@gmail.com>
2022-04-15 11:23:28 -0400

    wired up getVideoById gunction

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { useState } from 'react';
+import { getVideoById } from './helpers/index';
 
 import Header from './components/Header';
 import SearchVideoInput from './components/SearchVideoInput';
 
 const App = () => {
   const [searchValue, setSearchValue] = useState('');
+  const [video, setVideo] = useState([]);
   const [errorMessage, setErrorMessage] = useState(null);
 
   const handleChange = (e) => {
@@ -14,6 +16,8 @@ const App = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    getVideoById(searchValue, setVideo, setErrorMessage);
 
     setSearchValue('');
   };
