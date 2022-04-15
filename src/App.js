66f98d084eac25658baf87a3c383b82e559b278e
Shomari Roberts <shomariroberts@gmail.com>
2022-04-15 12:24:28 -0400

    rendered the video and video details on the page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { getVideoById } from './helpers/index';
 
 import Header from './components/Header';
 import SearchVideoInput from './components/SearchVideoInput';
+import { ReactComponent as Dream } from './imgs/zombieing.svg';
 
 const App = () => {
   const [searchValue, setSearchValue] = useState('');
@@ -31,6 +32,39 @@ const App = () => {
         handleSubmit={handleSubmit}
         errorMessage={errorMessage}
       />
+
+      {video.length === 0 ? (
+        <Dream
+          style={{
+            width: 'auto',
+            height: '22rem',
+            margin: '2rem auto',
+            opacity: '.10',
+          }}
+        />
+      ) : (
+        <div>
+          {video.map((v) => (
+            <div
+              key={v.id}
+              className='flex justify-start md:justify-center flex-wrap mt-8 drop-shadow-xl'
+            >
+              <div className='bg-slate-200 w-full md:w-5/12 h-96'>
+                <iframe
+                  src={`https://www.youtube.com/embed/${v.id}`}
+                  title={v.snippet.title}
+                  className='w-full h-full'
+                />
+              </div>
+
+              <div className='p-8 w-full md:w-4/12 h-96 md:mt-0 bg-white text-black overflow-y-auto'>
+                <h2 className='text-xl font-semibold'>{v.snippet.title}</h2>
+                <p className='mt-4'>{v.snippet.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
